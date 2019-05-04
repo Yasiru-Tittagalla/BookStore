b6a7f80712b616d69aae875faad5828437efd9a4
Yasiru <yasirutit1@gmail.com>
2019-05-04 17:18:26 +0530

    Book Store login

--- app/src/main/java/com/example/bookstore/SignUpActivity.java
+++ app/src/main/java/com/example/bookstore/SignUpActivity.java
@@ -1,4 +1,86 @@
 package com.example.bookstore;
 
-public class SignUpActivity {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText editTextPassword, editTextUserName;
+    Button signUpButton;
+    private FirebaseAuth mAuth;
+
+    @Override
+     protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        signUpButton = findViewById(R.id.buttonSignUp);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+    private void registerUser(){
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextUserName = (EditText) findViewById(R.id.editTextEmail);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        String email = editTextUserName.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Resgistra", Toast.LENGTH_SHORT).show();
+                        }
+                        /*else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }*/
+
+                        // ...
+                    }
+                });
+
+        if (email.isEmpty()) {
+            editTextUserName.setError("Email Is Required");
+            editTextUserName.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextUserName.setError("Please Enter A Valid E-mail");
+            editTextUserName.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password Is Required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum Length Of The Password Should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+    }
 }
