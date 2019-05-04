d2e9b1161b50aa94d0eb627996bdb76d75fec9cc
nirmalrv8 <nirmal.seneviratne@gmail.com>
2019-05-04 15:57:48 +0530

    added 4 separate pages for tabs

--- app/src/main/java/com/example/bookstore/DashboardActivity.java
+++ app/src/main/java/com/example/bookstore/DashboardActivity.java
@@ -91,41 +91,6 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -137,9 +102,22 @@ public class DashboardActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    Tab1HomeActivity tab1 = new Tab1HomeActivity();
+                    return tab1;
+                case 1:
+                    Tab2SearchActivity tab2 = new Tab2SearchActivity();
+                    return tab2;
+                case 2:
+                    Tab3FavoritesActivity tab3 = new Tab3FavoritesActivity();
+                    return tab3;
+                case 3:
+                    Tab4ReminderActivity tab4 = new Tab4ReminderActivity();
+                    return tab4;
+                default:
+                    return null;
+            }
         }
 
         @Override
---
+++ app/src/main/java/com/example/bookstore/Tab1HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.bookstore;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1HomeActivity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1_home, container, false);
+        return rootView;
+    }
+
+}
