4387979041c12b05b569d490a6e122babde1597d
Yasiru <yasirutit1@gmail.com>
2019-05-04 18:22:16 +0530

    Sign In Validations with firebase

--- app/src/main/java/com/example/bookstore/LoginActivity.java
+++ app/src/main/java/com/example/bookstore/LoginActivity.java
@@ -48,15 +48,26 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity  implements OnClickListener{
 
     TextView signUp;
+    Button logIn;
+    FirebaseAuth mAuth;
+    EditText editTextPassword, editTextUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
+        // Set up the login form
 
       findViewById(R.id.textViewSignup).setOnClickListener(this);
+      logIn = findViewById(R.id.buttonLogin);
+      logIn.setOnClickListener(new OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              doLogin();
+          }
 
+
+      });
     }
 
     @Override
@@ -66,6 +77,46 @@ switch (v.getId()){
         startActivity(new Intent(this, SignUpActivity.class));
     break;
 }
+    }
+    private void doLogin() {
+
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextUserName = (EditText) findViewById(R.id.editTextEmail);
+
+        String email = editTextUserName.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if (email.isEmpty()) {
+            editTextUserName.setError("Email Is Required");
+            editTextUserName.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextUserName.setError("Please Enter A Valid E-mail");
+            editTextUserName.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password Is Required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum Length Of The Password Should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                     startActivity(new Intent(this,dashboardActivity.class));
+                }
+                else
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/bookstore/SignUpActivity.java
+++ app/src/main/java/com/example/bookstore/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.bookstore;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -7,17 +8,22 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText editTextPassword, editTextUserName;
     Button signUpButton;
+    TextView signIn;
+    ProgressBar progressBar;
     private FirebaseAuth mAuth;
 
     @Override
@@ -26,7 +32,14 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         signUpButton = findViewById(R.id.buttonSignUp);
-
+        signIn = findViewById(R.id.textViewLogin);
+        progressBar =findViewById(R.id.progressbar);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,23 +55,28 @@ public class SignUpActivity extends AppCompatActivity {
 
         String email = editTextUserName.getText().toString();
         String password = editTextPassword.getText().toString();
-
+         progressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
                         if (task.isSuccessful()) {
-                            Toast.makeText(getApplicationContext(), "Resgistra", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "You Are Registerd Successfully", Toast.LENGTH_SHORT).show();
                         }
-                        /*else {
+                        else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }*/
+                            if( task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                Toast.makeText(SignUpActivity.this, "This E-mail Is Already Registered With Our Service",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                                Toast.makeText(SignUpActivity.this, task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                        }
 
-                        // ...
+                            // ...
+
                     }
                 });
 
