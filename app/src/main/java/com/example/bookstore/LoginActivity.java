2db2160f186c6ef7d9b73ff75940b9665802885c
Okazakikirito <okazakikirito@gmail.com>
2019-05-19 02:43:03 +0530

    Google Sign activity

--- app/src/main/java/com/example/bookstore/LoginActivity.java
+++ app/src/main/java/com/example/bookstore/LoginActivity.java
@@ -32,6 +32,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -51,16 +55,32 @@ public class LoginActivity extends AppCompatActivity  implements OnClickListener
 
     TextView signUp;
     Button logIn;
+    SignInButton signInButton;
     FirebaseAuth mAuth;
+    GoogleApiClient googleApiClient;
     EditText editTextPassword, editTextUserName;
+    private static  final  String TAG = "SignInActivity";
+    private static  final  int RC_SIGN_IN = 9001;
+    GoogleApiClient.OnConnectionFailedListener ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form
 
+        // Set up the login form
       findViewById(R.id.textViewSignup).setOnClickListener(this);
+      signInButton = findViewById(R.id.googleSignIn);
+      signInButton.setOnClickListener(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, ad)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
       logIn = findViewById(R.id.buttonLogin);
       logIn.setOnClickListener(new OnClickListener() {
           @Override
@@ -77,9 +97,25 @@ public class LoginActivity extends AppCompatActivity  implements OnClickListener
 switch (v.getId()){
     case R.id.textViewSignup:
         startActivity(new Intent(this, SignUpActivity.class));
+        break;
+    case R.id.googleSignIn:
+googleSignIn();
     break;
 }
     }
+
+    private void googleSignIn() {
+
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+    }
+
     private void doLogin() {
 
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
