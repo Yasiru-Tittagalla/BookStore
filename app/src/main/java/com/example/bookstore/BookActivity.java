9d9ba5caafbef166ebdc0add9650ee77220cff56
nirmal.seneviratne <nirmal.seneviratne@gmail.com>
2019-05-20 00:50:35 +0530

    wishlist dialog for home tab

--- app/src/main/java/com/example/bookstore/BookActivity.java
+++ app/src/main/java/com/example/bookstore/BookActivity.java
@@ -1,5 +1,7 @@
 package com.example.bookstore;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +10,11 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -51,13 +56,39 @@ public class BookActivity extends AppCompatActivity {
             }
         });
 
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         wishlistbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent wishIntent = new Intent(v.getContext() ,WishListDialog.class);
-                wishIntent.putExtra("userName", LoginActivity.userEmail);
-//                wishIntent.putExtra("ID",id);
-                startActivity(wishIntent);
+                final EditText input = new EditText(getApplicationContext());
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                builder.setView(input);
+                //Setting message manually and performing action on button click
+                builder.setMessage("Please add a wishlist note")
+                        .setCancelable(true)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+//                                    finish();
+                                Toast.makeText(getApplicationContext(),input.getText(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //  Action for 'NO' Button
+                                dialog.cancel();
+                                Toast.makeText(getApplicationContext(),"you choose no action for alertbox",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("Adding to wishlist");
+                alert.show();
             }
         });
 
